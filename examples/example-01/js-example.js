09712690e8ddf66abab855ff95a4feff11f067a6
yoanatanblox <94914141+yoanatanblox@users.noreply.github.com>
2022-08-15 16:33:47 +0300

    Updated comments an Variables
    Updated some things here let me know if its any problem

--- examples/example-01/js-example.js
+++ examples/example-01/js-example.js
@@ -6,20 +6,20 @@ const operatorIds = require('./operatorIds.json');
 const keystorePassword = 'testtest';
 
 async function main() {
-  // Step 1: read keystore file
+  // Step 1: read validator private key form keystore file
   const ssvKeys = new SSVKeys();
   const privateKey = await ssvKeys.getPrivateKeyFromKeystoreData(keystore, keystorePassword);
 
-  // Step 2: Build shares from operator IDs and public keys
+  // Step 2: Build keyshares from operator IDs and keys
   const threshold = await ssvKeys.createThreshold(privateKey, operatorIds);
-  const shares = await ssvKeys.encryptShares(operators, threshold.shares);
+  const keyshares = await ssvKeys.encryptShares(operators, threshold.shares);
 
   // Step 3: Build final web3 transaction payload
   const payload = await ssvKeys.buildPayload(
     threshold.validatorPublicKey,
     operatorIds,
-    shares,
-    123456789,
+    keyshares, // shares public keys + shares encrypted
+    123456789, // amount of ssv tokens in wei to deposit
   );
 
   console.debug('payload: ', payload);
