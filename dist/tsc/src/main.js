3ba37e2681c306ca82d0f6c73fd75be127f42c5a
Dmitri Meshin <dmitri@blox.io>
2022-08-16 11:06:38 +0300

    IO1-1522: Export key shares data and payload

--- dist/tsc/src/main.js
+++ dist/tsc/src/main.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.InvalidOperatorKeyException = exports.Encryption = exports.KeySharesDataV2 = exports.KeyShares = exports.EthereumKeyStore = exports.Threshold = exports.SSVKeys = void 0;
+exports.InvalidOperatorKeyException = exports.Encryption = exports.KeyShares = exports.KeySharesDataV2 = exports.EthereumKeyStore = exports.Threshold = exports.SSVKeys = void 0;
 const tslib_1 = require("tslib");
 var SSVKeys_1 = require("./lib/SSVKeys");
 Object.defineProperty(exports, "SSVKeys", { enumerable: true, get: function () { return SSVKeys_1.SSVKeys; } });
@@ -8,9 +8,9 @@ var Threshold_1 = require("./lib/Threshold/Threshold");
 Object.defineProperty(exports, "Threshold", { enumerable: true, get: function () { return tslib_1.__importDefault(Threshold_1).default; } });
 var EthereumKeyStore_1 = require("./lib/EthereumKeyStore");
 Object.defineProperty(exports, "EthereumKeyStore", { enumerable: true, get: function () { return tslib_1.__importDefault(EthereumKeyStore_1).default; } });
+exports.KeySharesDataV2 = tslib_1.__importStar(require("./lib/KeyShares/KeySharesData/KeySharesDataV2"));
 var KeyShares_1 = require("./lib/KeyShares/KeyShares");
 Object.defineProperty(exports, "KeyShares", { enumerable: true, get: function () { return KeyShares_1.KeyShares; } });
-exports.KeySharesDataV2 = tslib_1.__importStar(require("./lib/KeyShares/KeySharesData/KeySharesDataV2"));
 var Encryption_1 = require("./lib/Encryption/Encryption");
 Object.defineProperty(exports, "Encryption", { enumerable: true, get: function () { return tslib_1.__importDefault(Encryption_1).default; } });
 Object.defineProperty(exports, "InvalidOperatorKeyException", { enumerable: true, get: function () { return Encryption_1.InvalidOperatorKeyException; } });
