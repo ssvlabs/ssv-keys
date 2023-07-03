65925cd335d23aca18d8edfc83f734e58a8a5e1a
guy muroch <guym@blox.io>
2023-07-03 14:37:13 +0300

    adding support for v2

--- examples/server-worker/index.ts
+++ examples/server-worker/index.ts
@@ -12,12 +12,18 @@ const ssvKeys = new SSVKeys();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+const nonce = 0;
 app.post('/key-shares/generate', async (req: Request, res: Response) => {
   const operators_ids = String(req.body['operators_ids'] || '')
     .split(',')
     .map((id) => Number(id.trim()))
     .filter(id => !!id);
+
+  if (!operators_ids.length) {
+    return res
+      .status(constants.HTTP_STATUS_BAD_REQUEST)
+      .json({ message: 'Operator IDs required' });
+  }
 
   if (!operators_ids.length) {
     return res
@@ -39,9 +45,18 @@ app.post('/key-shares/generate', async (req: Request, res: Response) => {
   const keystore = req.body['keystore'];
 
   if (!keystore) {
+
     return res
       .status(constants.HTTP_STATUS_BAD_REQUEST)
       .json({ message: 'Keystore is required' });
+  }
+
+  const owner_address = String(req.body['owner_address'] || '')
+
+  if (!owner_address.length) {
+    return res
+      .status(constants.HTTP_STATUS_BAD_REQUEST)
+      .json({ message: 'Owner address is required' });
   }
 
   const password = String(req.body['password'] || '');
@@ -65,31 +80,22 @@ app.post('/key-shares/generate', async (req: Request, res: Response) => {
   const keyShares = new KeyShares();
   keyShares.update({ operators, publicKey });
 
-  // The nonce of the owner within the SSV contract (increments after each validator registration), obtained using the ssv-scanner tool
-  const TEST_OWNER_NONCE = 1;
-  // The cluster owner address
-  const TEST_OWNER_ADDRESS = '0x81592c3de184a3e2c0dcb5a261bc107bfa91f494';
-
   await keyShares.buildPayload({
     publicKey,
     operators,
     encryptedShares,
   },{
-    ownerAddress: TEST_OWNER_ADDRESS,
-    ownerNonce: TEST_OWNER_NONCE,
+    ownerAddress: owner_address,
+    ownerNonce: nonce,
     privateKey
   });
-  console.log('<<<<<<<<<<<<<<<<<<<<<<<<<newPayload-guy>>>>>>>>>>>>>>>>>>>>>>>>>');
-  console.log('<<<<<<<<<<<<<<<<<<<<<<<<<newPayload-guy>>>>>>>>>>>>>>>>>>>>>>>>>');
   keyShares.payload.readable.encryptedKeys = encryptedShares.map((share: EncryptShare) => share.privateKey);
   keyShares.payload.readable.publicKeys = encryptedShares.map((share: EncryptShare) => share.publicKey);
-  console.log('<<<<<<<<<<<<<<<<<<<<<<<<<newPayload-guy>>>>>>>>>>>>>>>>>>>>>>>>>');
 
   console.log(`Built key shares for operators: ${String(operators_ids)} and public key: ${keystore.pubkey}`);
   res.json(JSON.parse(keyShares.toJson()));
 });
 
 app.listen(port, () => {
-  console.log('brotherit!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
