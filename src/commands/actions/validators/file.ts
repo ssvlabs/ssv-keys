d2ca7192bb2a186cf362da4ddaec1ade043ede0d
Vadim <vadim.inc@gmail.com>
2023-09-10 14:58:53 +0200

    fix lint

--- src/commands/actions/validators/file.ts
+++ src/commands/actions/validators/file.ts
@@ -48,7 +48,7 @@ export const sanitizePath = (inputPath: string): string => {
   // Remove any characters that are not typically allowed or are problematic in file paths.
   // Here, we're allowing alphanumeric characters, spaces, hyphens, underscores, and periods.
   // You can adjust the regex as needed.
-  sanitizedPath = sanitizedPath.replace(/[^a-zA-Z0-9_\-\.\/\\ ]/g, '');
+  sanitizedPath = sanitizedPath.replace(/[^a-zA-Z0-9_\-./\\ ]/g, '');
 
   // On Windows, paths might start with a drive letter. We can check and ensure it's a valid drive letter.
   if (process.platform === 'win32') {
