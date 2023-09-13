476944459376f4e64d3f02142f083a38e71d0d31
Vadim <vadim.inc@gmail.com>
2023-09-13 06:53:36 +0200

    update help messages

--- src/commands/actions/arguments/keystore-path.ts
+++ src/commands/actions/arguments/keystore-path.ts
@@ -9,11 +9,12 @@ export default {
   options: {
     required: true,
     type: String,
-    help: 'The validator keystore file(s) path'
+    help: 'The validator keystore file/folder path, if a folder is provided all keystore files within the provided folder will be split according to the provided arguments'
   },
   interactive: {
     options: {
       type: 'text',
+      message: 'Provide the keystore file path',
       validateSingle: (filePath: string): any => {
         filePath = sanitizePath(String(filePath).trim());
         let isValid = fileExistsValidator(filePath);
--- src/commands/actions/arguments/password.ts
+++ src/commands/actions/arguments/password.ts
@@ -6,11 +6,12 @@ export default {
   options: {
     required: true,
     type: String,
-    help: 'The keystore file encryption password'
+    help: 'The keystore file encryption password, if a folder was provided the password will be used for all keystore files in the folder'
   },
   interactive: {
     options: {
       type: 'password',
+      message: 'Provide the keystore file password',
       /*
       validate: async (password: string): Promise<boolean | string> => {
         return await keystorePasswordValidator.validatePassword(password, true);
