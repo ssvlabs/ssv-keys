d68aec1465b62aad99546d70425e4cb4339bb520
Ilya <ilya@blox.io>
2024-11-24 13:34:19 +0200

    bug fix - "or" instead "and"

--- src/lib/SSVKeys.ts
+++ src/lib/SSVKeys.ts
@@ -144,11 +144,11 @@ export class SSVKeys {
     }
 
     return {
-      isValid: !!sharesError && !!signatureError && !!errorMessage,
+      isValid: !!sharesError || !!signatureError || !!errorMessage,
       isSharesValid: !!sharesError,
       sharesPublicKeys: restoredSharesPublicKeys,
       encryptedKeys: restoredSharesEncryptedKeys,
-      memo: !!sharesError && !!signatureError ? [{
+      memo: !!sharesError || !!signatureError ? [{
         message: errorMessage,
         error: sharesError || signatureError,
         data: `${sharesErrorMessage}${signatureErrorMessage ? '. ' + signatureErrorMessage : ''}`,
